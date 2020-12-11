5e0b90697fc0e69515b1ddbb980e31e29bac0034
Adam Hansen <onero@users.noreply.github.com>
2020-12-11 17:34:31 +0100

    Setup project

---
+++ server/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const yup = require('yup');
+const { nanoid } = require('nanoid');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+
+const schema = yup.object().shape({
+  // Any characters, digits, underscore and dash
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'a Url Shortener - Shortens urls for your convenience!',
+  });
+});
+
+app.get('/:id', (req, res) => {
+  // TODO ALH: Redirect to url
+});
+
+app.post('/:url', async (req, res, next) => {
+  let { slug, url } = req.body;
+
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    }
+    slug = slug.toLowerCase();
+    res.json({
+      slug,
+      url
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/url/:id', (req, res) => {
+  // TODO ALH: Get url information
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'UhUh!' : error.stack,
+  });
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});

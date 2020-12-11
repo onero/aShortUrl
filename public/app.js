35004a5183146c1f5d23db1e7b66ba064e38c01f
Adam Hansen <onero@users.noreply.github.com>
2020-12-11 23:36:36 +0100

    Added vue app

---
+++ public/app.js
@@ -0,0 +1,23 @@
+const app = new VTTCue({
+  el: '#app',
+  data: {
+    url: '',
+    slug: ''
+  },
+  methods: {
+    async createUrl() {
+      console.log(this.url, this.slug)
+      const response = await fetch('/url', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: this.url,
+          slug: this.slug
+        })
+      });
+      this.created = await response.json();
+    }
+  }
+})
